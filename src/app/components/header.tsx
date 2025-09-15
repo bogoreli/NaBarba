9b0e1bb2a79f2e80c9d0a45bda7fc9c967f4912c
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2025-09-15 16:51:19 -0300

    Chore: add setup shadcn, next and header navigation

---
+++ src/app/components/header.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { MenuIcon } from "lucide-react"
+import Image from "next/image"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-2">
+        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Button variant="outline" size="icon">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
