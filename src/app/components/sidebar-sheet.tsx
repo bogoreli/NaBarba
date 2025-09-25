b3f33643e2045b23a2399ad8aa5b3ab86818cd45
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2025-09-25 09:20:19 -0300

    Feat: create login with Google component

--- src/app/components/sidebar-sheet.tsx
+++ src/app/components/sidebar-sheet.tsx
@@ -4,7 +4,15 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { DialogTitle } from "@radix-ui/react-dialog"
 
 const SidebarSheet = () => {
   return (
@@ -13,8 +21,28 @@ const SidebarSheet = () => {
         <SheetTitle>Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid p-2">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid p-2">
+        <h2 className="text-lg font-bold">Olá faça o seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma!</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button className="font-bold">
+              <Image alt="Login com Google" src={"/google.svg"} width={18} height={18} />
+              Entrar com Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage
             src={
               "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -24,7 +52,7 @@ const SidebarSheet = () => {
         <div>
           <p className="font-bold">Kevin bogoreli</p>
           <p className="text-xs">bogoreli.kevin@outlook.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-4 border-b border-solid p-2">
