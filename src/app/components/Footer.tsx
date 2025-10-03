ca8b8175eef4f46fea4ca9b98f309fc2ad9b52e9
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2025-10-03 15:27:02 -0300

    fix: add margin top to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "@/components/ui/card"
 const Footer = () => {
   return (
     <footer>
-      <Card>
+      <Card className="mt-6">
         <CardContent>
           <p className="text-sm text-gray-500">
             © 2025 NaBarba. Todos os direitos reservados.
